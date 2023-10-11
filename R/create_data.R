763d01fa90d0d562275a45fd4209d679aab2b5f6
munchfab <fabian.felix.muench@uni-jena.de>
2023-10-11 12:50:51 +0200

    Some simplifications on function

--- R/create_data.R
+++ R/create_data.R
@@ -17,10 +17,8 @@ create_dsem_data <- function(data, delta, id, time) {
 
   # create empty list for imputed data frames
   imp_list <- list()
-
   # create numeric id variable
   data$num_id <- as.numeric(as.factor(data[, id]))
-
   # loop over obersavtions in data set
   for (i in 1:length(unique(data$num_id))) {
     # store continuous time variable for id == i and have it start with 0
@@ -33,7 +31,6 @@ create_dsem_data <- function(data, delta, id, time) {
       # otherwise just round up
       ceiling(diff_cont_time / delta)
     )
-    # create temporary data frame continuous and integer time variables
     temp <- data.frame(diff_cont_time, int_time)
     # calculate difference between integer time values, starting with 0
     temp$diff_int_time <- c(0, diff(int_time))
@@ -43,22 +40,18 @@ create_dsem_data <- function(data, delta, id, time) {
     # store at which position the difference of time_int is greater 0
     # (i.e., integer time is not in same interval)
     nonoverlap <- which(diff(int_time) > 1) + 1
-    # create indicator variables for overlapping time intervals
+    # create indicator variables for (non-)overlapping time intervals
     temp[overlap, "overlap"] <- 1
-    # create indicator variables for non-overlapping time intervals
     temp[nonoverlap, "nonoverlap"] <- 1
-    # create new shifted time variable fo counting number of shifts
     temp$int_time_shifted <- temp$int_time
     # count number of times where observations are in same interval
     n_overlap <- length(overlap)
-
     # if there is overlap, shift the obversavations as in Hamaker (2018)
     if (n_overlap > 0) {
       # start counter
       k <- 1
       # repeat shifting until k > n_overlap
       repeat {
-        # no idea whats happening here
         min <- overlap[1]
         max <- nonoverlap[which(nonoverlap > overlap[1])][1]
         temp[min:(max - 1), "int_time_shifted"] <-
@@ -67,13 +60,11 @@ create_dsem_data <- function(data, delta, id, time) {
         nonoverlap <- which(diff(temp$int_time_shifted) > 1) + 1
         nonoverlap[length(nonoverlap) + 1] <- dim(temp)[1] + 1
         k <- k + 1
-
         if (k > n_overlap) {break}
       }
     }
-    # check whether or not shifted more than 4 intervals
+    # check whether or not shifted more than 4 intervals and print warning
     temp$shifts <- temp$int_time_shifted - temp$int_time
-    # if so, print warning
     if (max(temp$shifts) > 4) {
       print(paste("Warning: obs shifted", temp$shifts,
                   "intervals for id", id))
@@ -82,17 +73,15 @@ create_dsem_data <- function(data, delta, id, time) {
     temp$int_time_shifted <- temp$int_time_shifted -
       min(temp$int_time_shifted) + 1
     temp[, time] <- data[data$num_id == i, time]
-    # insert missing
+    # insert missings
     new <- data.frame(num_id = i,
                       int_time = seq(1:temp[dim(temp)[1], "int_time"]))
 
     # store in list
     imp_list[[i]] <- merge(new, temp, by = "int_time", all = TRUE)
   }
-
   # reduce to data frame
   imp_data <- purrr::reduce(imp_list, rbind)
-
   # join with existing data set
   dsem_data <- dplyr::full_join(
     imp_data, data,
@@ -102,10 +91,5 @@ create_dsem_data <- function(data, delta, id, time) {
   sem_data <- dplyr::group_by(.data = dsem_data, num_id)
   dsem_data <- tidyr::fill(dsem_data, id, .direction = "downup")
   dsem_data <- dplyr::ungroup(dsem_data)
-  # want to do with pipe but doesnt work
-  # dsem_data <- dplyr::group_by(.data = dsem_data, num_id) %>%
-  #   tidyr::fill(id, .direction = "downup") %>%
-  #   dplyr::ungroup(dsem_data)
-
   return(dsem_data)
 }
