123c6a912a617d7ad1298e6c285118fe47e5dac1
munchfab <fabian.felix.muench@uni-jena.de>
2023-11-17 13:05:55 +0100

    Use pipe

---
+++ R/dsemr-package.R
@@ -0,0 +1,12 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom magrittr %>%
+#' @import Rcpp
+#' @import methods
+#' @importFrom rstan sampling
+#' @import Rcpp
+#' @useDynLib dsemr, .registration = TRUE
+## usethis namespace: end
+NULL
