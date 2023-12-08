b7554350f21f5d46d271eef86280eb2894c39a2f
munchfab <fabian.felix.muench@uni-jena.de>
2023-12-08 11:10:43 +0100

    Updatet for manifest AR model

--- R/dsem.R
+++ R/dsem.R
@@ -1,22 +1,37 @@
-dsem <- function(formula = NULL, data,
-                 y, id, beep,
+#' Title
+#'
+#' @param formula
+#' @param data
+#' @param y
+#' @param id
+#' @param beep
+#' @param iter
+#' @param seed
+#' @param miss_handling
+#'
+#' @return
+#' @export
+#'
+#' @examples
+dsem <- function(formula, data,
+                 y = NULL, id = NULL, beep = NULL,
                  iter = 2000, seed = NULL,
                  miss_handling = "remove") {
 
-  dsem_terms <- terms(formula)
-  mf <- model.frame(dsem_terms, data = data)
+  # parse formula for variables to be passed to Stan
+  dsem_terms <- dsem_formula(formula)
 
-  return(dsem_terms)
-
+  # create Stan data from parsed formula
   stan_data <- create_stan_data(
     data = data,
-    y = y,
-    id = id,
-    beep = beep,
+    y = dsem_terms$response,
+    id = dsem_terms$id,
+    beep = dsem_terms$beep,
     miss_handling = miss_handling
   )
 
   # parameters to monitor
+  # needs updating for different models
   pars <- c("btw_pred", # fixed effects of mu, ar, and (log) innovation variance
             "sigma", # random effect SDs
             "bcorr", # random effect correlations
@@ -24,18 +39,21 @@ dsem <- function(formula = NULL, data,
             "y_rep")
 
 
+  # draw samples for respective model
+  # if latent variables are present, use latent variable model
+  if (grepl("lv", specials)) {
+    NULL
+  } else if (grepl("ar", specials)) {
+    fit <- rstan::sampling(
+      object = stanmodels$manifest_AR,
+      chains = 4,
+      cores = 4,
+      iter = iter,
+      data = stan_data,
+      seed = seed,
+      pars = pars
+    )
+  }
 
-  # draw samples
-  # fit <- rstan::sampling(
-  #   object = stanmodels$manifest_AR,
-  #   chains = 4,
-  #   cores = 4,
-  #   iter = iter,
-  #   data = stan_data,
-  #   seed = seed,
-  #   pars = pars
-  # )
-
-  # return(fit)
-
+  return(fit)
 }
