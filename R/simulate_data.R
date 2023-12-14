a158b00ed404667759f128c3663e2060cbe5469d
munchfab <fabian.felix.muench@uni-jena.de>
2023-12-14 15:48:58 +0100

    Add simulate_data function for latent AR model

--- R/simulate_data.R
+++ R/simulate_data.R
@@ -1,3 +1,4 @@
+# manifest
 sim_data_AR1_man <- function(N, TP,
                              mu, phi, logv,
                              sigma_mu, sigma_phi, sigma_logv,
@@ -67,3 +68,107 @@ sim_data_AR1_man <- function(N, TP,
   }
   return(within)
 }
+
+# latent
+sim_data_AR1_lat <- function(N, TP, IND,
+                             lambda_w, sigma_w,
+                             lambda_b, alpha, sigma_b,
+                             mu, phi, logv,
+                             sigma_mu, sigma_phi, sigma_logv,
+                             mu_ec, sigma_ec,
+                             cor_mu_phi, cor_mu_logv, cor_phi_logv,
+                             cor_mu_ec, cor_phi_ec, cor_logv_ec,
+                             miss = 0,
+                             seed = NULL,
+                             burnin = 100) {
+
+  # mus
+  bmus <- c(mu, phi, logv, mu_ec)
+
+  # create covariance matrix from inputs
+  cov_mx <- diag(c(sigma_mu^2, sigma_phi^2, sigma_logv^2, sigma_ec^2))
+
+  # within-level
+  lambda_w <- lambda_w # factor loadings
+  sigma_w <- diag(sigma_w) # covariance matrix of measurement errors
+
+  # create between-level data
+  lambda_b <- lambda_b
+  alphas <- matrix(alpha, ncol = 1) # item intercepts
+  sigma_b <- diag(sigma_b)   # covariance matrix of measurement errors
+
+  # draw person parameter from multivariate normal distribution
+  btw <- mnormt::rmnorm(n = N, mean = bmus, varcov = cov_mx)
+
+  # generate latent process
+  eta_w <- list()
+
+  for (pp in 1:N) {
+    eta_w[[pp]] <- c(1:TP)
+    eta_w[[pp]][1] <- rnorm(1, 0, sqrt(exp(btw[pp,3])))
+    for (tt in 2:TP) {
+      eta_w[[pp]][tt] <- btw[pp, 2] * eta_w[[pp]][tt - 1] + rnorm(1, 0, sqrt(exp(btw[pp, 3])))
+    }
+  }
+
+  # check if data are reasonable
+  # psych::autoR(etaW[[1]])
+  # btw[1,2]
+  # summary(lm(eta_w[[1]][2:TP] ~ eta_w[[1]][1:(TP - 1)]))$sigma
+  # sqrt(exp(btw[1, 3]))
+
+  # generate data
+  y <- array(NA, dim = c(N, TP, IND))
+
+  for(pp in 1:N) {
+    # sample error vectors
+    # within
+    eps_w <- mnormt::rmnorm(n = TP, rep(0, IND), sigma_w) # sample error vector
+    y_w <- matrix(NA, ncol = IND, nrow = TP)
+    y_w <- matrix(eta_w[[pp]], ncol = 1) %*% t(cbind(lambda_w)) + eps_w
+
+    eps_b <- mnormt::rmnorm(1, rep(0, IND), sigma_b) # sample error vector
+    y_b <- matrix(NA, ncol = IND, nrow = 1)
+    y_b <- t(alphas) + matrix(btw[pp, 1], ncol = 1) %*% t(cbind(lambda_b)) + eps_b
+
+    y[pp, , ] <- matrix(y_b, nrow = TP, ncol = IND, byrow = T) + y_w
+  }
+
+  # retransform data
+  y_new <- array(data = 0, dim = c(IND, N, TP))
+
+  for (ii in 1:IND) {
+    for (pp in 1:N) {
+      for (tt in 1:TP) {
+        y_new[ii, pp, tt] <- y[pp, tt, ii]
+      }
+    }
+  }
+
+  # bring data into long format data frame
+  df <- data.frame(
+    id = rep(1:N, each = TP),
+    TP = rep(1:TP, times = N)
+  )
+
+  for (ii in 1:IND) {
+    for (pp in 1:N) {
+      for (tt in 1:TP) {
+        df[df$id == pp & df$TP == tt, 2 + ii] <- y_new[ii, pp, tt]
+      }
+    }
+  }
+
+  # check if retranformation was correct
+  # mean(df$V3[df$N == 2])
+  # mean(y_new[1, 2, ])
+  # mean(df$V5[df$TP == 5])
+  # mean(y_new[3, , 5])
+
+  # proper column names
+  colnames(df) <- c("id", "TP", paste0("y", 1:IND))
+
+  # return data and parameter inputs
+  return(df)
+}
+
