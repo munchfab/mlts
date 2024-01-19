a21e76817d2d98a207bc926a6d3683036ccba9d6
munchfab <fabian.felix.muench@uni-jena.de>
2024-01-19 19:09:38 +0100

    Implement q > 1 in between-model.

--- R/VARmodelPaths.R
+++ R/VARmodelPaths.R
@@ -45,18 +45,18 @@ VARmodelPaths <- function(VARmodel, data = NULL, labels = NULL) {
     % draw decomposition
     \\node [manifest] (y1t) {$y_{1,t}$};
     \\node [latent]   (y1wt)    [above = 2.5em of y1t]  {$y_{1,t}^w$};
-    \\node [latent]   (mu_y1)   [below = 2.5em of y1t]  {$\\mu_{y_1,t}$};
+    \\node [latent]   (mu_1)   [below = 2.5em of y1t]  {$\\mu_{1}$};
 
     % draw paths
     \\draw [path] (y1wt) to node [] {} (y1t);
-    \\draw [path] (mu_y1) to node [] {} (y1t);"
-  } else {
+    \\draw [path] (mu_1) to node [] {} (y1t);"
+  } else { # for q > 1
     dc <- paste0(
     "
     % draw decomposition
     \\node [manifest] (y1t) {$y_{1,t}$};
     \\node [latent]   (y1wt)    [above = 2.5em of y1t]  {$y_{1,t}^w$};
-    \\node [latent]   (mu_y1)   [below = 2.5em of y1t]  {$\\mu_{y_1,t}$};
+    \\node [latent]   (mu_1)   [below = 2.5em of y1t]  {$\\mu_{1}$};
 
     % draw nodes
     \\foreach \\i [remember = \\i as \\lasti (initially 1)] in {2, ..., ",
@@ -69,7 +69,7 @@ VARmodelPaths <- function(VARmodel, data = NULL, labels = NULL) {
 
     \\foreach \\i [remember = \\i as \\lasti (initially 1)] in {2, ...,",
     VARmodel$q, "}
-    \\node [latent] (mu_y\\i) [below = 2.5em of y\\i t] {$\\mu_{y_{\\i,t}}$};
+    \\node [latent] (mu_\\i) [below = 2.5em of y\\i t] {$\\mu_{\\i}$};
 
     % draw paths
     \\foreach \\i [remember = \\i as \\lasti (initially 1)] in {1, ...,",
@@ -78,7 +78,7 @@ VARmodelPaths <- function(VARmodel, data = NULL, labels = NULL) {
 
     \\foreach \\i [remember = \\i as \\lasti (initially 1)] in {1, ...,",
     VARmodel$q, "}
-    \\draw [path] (mu_y\\i) to node [] {} (y\\i t);
+    \\draw [path] (mu_\\i) to node [] {} (y\\i t);
     "
     )
   }
@@ -119,22 +119,22 @@ VARmodelPaths <- function(VARmodel, data = NULL, labels = NULL) {
         model[model$Param == "phi_11" & model$Type == "Fix effect", "isRandom"] == 1,
         ", postaction = random]", "]"
       ),
-      " (y1wt-1)  to node [] {$\\phi_{y_1}$}  (y1wt);"
+      " (y1wt-1)  to node [] {$\\phi_{1}$}  (y1wt);"
     )
     ln.sigma2 <- paste0(
       "\\draw [var", ifelse(
         model[model$Param == "ln.sigma2_1" & model$Type == "Fix effect", "isRandom"] == 1,
         ", postaction = random]", "]"
       ),
-      " (delta1t.120)   to node [] {$\\pi_{y_1}$} (delta1t.60);"
+      " (delta1t.120)   to node [] {$\\pi_{1}$} (delta1t.60);"
     )
-  } else {
+  } else { # for q > 1
     wm <- paste0(
     "
     % draw within-level structural model
     \\node [latent] (y1wt-1)  {$y_{1,t-1}^w$};
     \\node [latent] (y1wt)    [right =5em of y1wt-1]  {$y_{1,t}^w$};
-    \\node [latent] (delta1t)   [right =2.5em of y1wt]  {$\\delta_{{y_1},t}$};
+    \\node [latent] (delta1t)   [right =2.5em of y1wt]  {$\\delta_{1,t}$};
 
     % draw nodes
     \\foreach \\i [remember = \\i as \\lasti (initially 1)] in {2, ..., ",
@@ -147,7 +147,7 @@ VARmodelPaths <- function(VARmodel, data = NULL, labels = NULL) {
 
     \\foreach \\i [remember = \\i as \\lasti (initially 1)] in {2, ..., ",
     VARmodel$q, "}
-    \\node [latent] (delta\\i t) [below = 2.5em of delta\\lasti t] {$\\delta_{{y_\\i} ,t}$};
+    \\node [latent] (delta\\i t) [below = 2.5em of delta\\lasti t] {$\\delta_{\\i ,t}$};
 
     % draw paths from residuals to yiwt
     \\foreach \\i [remember = \\i as \\lasti (initially 1)] in {1, ...,",
@@ -173,7 +173,7 @@ VARmodelPaths <- function(VARmodel, data = NULL, labels = NULL) {
                 all_phis[all_phis$Param == paste0("phi_", i, j), "isRandom"] == 1,
                 ", postaction = random]", "]"
               ),
-              "  (y", i, "wt-1)  to node  []  {$\\phi_{y_", i, "}$}  (y", i, "wt);\n"
+              "  (y", i, "wt-1)  to node  []  {$\\phi_{", i, j, "}$}  (y", i, "wt);\n"
             )
           )
         } else {
@@ -185,7 +185,7 @@ VARmodelPaths <- function(VARmodel, data = NULL, labels = NULL) {
                 all_phis[all_phis$Param == paste0("phi_", i, j), "isRandom"] == 1,
                 ", postaction = random_cl]", "]"
               ),
-              "  (y", i, "wt-1)  to node  [pos = .2]  {$\\phi_{y_{", i, j, "}}$}  (y", j, "wt);\n"
+              "  (y", i, "wt-1)  to node  [pos = .2]  {$\\phi_{", i, j, "}$}  (y", j, "wt);\n"
             )
           )
         }
@@ -206,12 +206,12 @@ VARmodelPaths <- function(VARmodel, data = NULL, labels = NULL) {
             all_sigmas[all_sigmas$Param == paste0("ln.sigma2_", i), "isRandom"] == 1,
             ", postaction = random]", "]"
           ),
-          "  (delta", i, "t.120)   to node [] {$\\pi_{y_", i, "}$} (delta", i, "t.60);\n"
+          "  (delta", i, "t.120)   to node [] {$\\pi_{", i, "}$} (delta", i, "t.60);\n"
         )
       )
     }
     # do the same for innovation covariances
-    # all_psis <- model[grepl("ln.sigma_", model$Param) & grepl("Fix", model$Type), ]
+    all_psis <- model[grepl("ln.sigma_", model$Param) & grepl("Fix", model$Type), ]
     # n_psi <- nrow(all_psis)
     psi_vec <- c()
     if (VARmodel$q == 1) {
@@ -221,7 +221,7 @@ VARmodelPaths <- function(VARmodel, data = NULL, labels = NULL) {
           all_psis[all_psis$Param == "ln.sigma_12", "isRandom"] == 1,
           ", postaction = random]", "]"
         ),
-        "  (delta1t.0)   to node [] {$\\pi_{y_{12}}$} (delta2t.0);\n"
+        "  (delta1t.0)   to node [] {$\\pi_{12}$} (delta2t.0);\n"
       )
     } else {
       for (i in 1:(VARmodel$q - 1)) {
@@ -233,7 +233,7 @@ VARmodelPaths <- function(VARmodel, data = NULL, labels = NULL) {
                 all_psis[all_psis$Param == paste0("ln.sigma_", i, j), "isRandom"] == 1,
                 ", postaction = random]", "]"
               ),
-              "  (delta", i, "t.0)   to node [] {$\\pi_{y_{", i, j, "}}$} (delta", j, "t.0);\n"
+              "  (delta", i, "t.0)   to node [] {$\\pi_{", i, j, "}$} (delta", j, "t.0);\n"
             )
           )
         }
@@ -262,45 +262,124 @@ VARmodelPaths <- function(VARmodel, data = NULL, labels = NULL) {
 
   # between-model #############################################################
 
-  # # between-model caption
-  # bm_caption <- "\\caption*{Between-model.}"
-  #
-  # # draw nodes for q time-series constructs
-  # if (VARmodel$q == 1) {
-  #   bm <- "
-  #   % draw between-level structural model
-  #   \\node [latent] (mu_y1) {$\\mu_{y_1,t}$};
-  #   \\node [latent] (phi_y1) [right = 1.5em of mu_y1] {$\\phi_{y_1}$};
-  #   \\node [latent] (lnsigma2_y1) [right = 1.5em of phi_y1] {$log(\\pi_{y_1})$};
-  #   "
-  #   # draw (co-)variances conditional on existing correlations
-  #   if (nrow(model[model$Param == "r_mu_1.phi_11"]) > 0) {
-  #     r_mu_1.phi_11 <- "\\draw [cov] (mu_y1.south) to node [] {} (phi_y1.south);"
-  #   }
-  #   if (nrow(model[model$Param == "r_mu_1.ln.sigma2_1"]) > 0) {
-  #     r_mu_1.ln.sigma2_1 <- "\\draw [cov] (mu_y1.south) to node [] {} (lnsigma2_y1.south);"
-  #   }
-  #   if (nrow(model[model$Param == "r_phi_11.ln.sigma2_1"]) > 0) {
-  #     r_phi_11.ln.sigma2_1 <- "\\draw [cov] (phi_y1.south) to node [] {} (lnsigma2_y1.south);"
-  #   }
-  # }
-  #
-  # # paste together
-  # between_model <- paste(bm, r_mu_1.phi_11, r_mu_1.ln.sigma2_1, r_phi_11.ln.sigma2_1, sep = "\n")
-  #
-  # # paste together
-  # between_model <- paste(
-  #   begin_figure,
-  #   bm_caption,
-  #   begin_tikz,
-  #   between_model,
-  #   end_tikz,
-  #   end_figure,
-  #   sep = "\n"
-  # )
-  #
-  # # paste within_model to markdown
-  # cat(between_model, file = "pathmodel.rmd", append = TRUE)
+  # between-model caption
+  bm_caption <- "\\caption*{Between-model.}"
+
+  # draw nodes for q time-series constructs
+  if (VARmodel$q == 1) {
+    bm <- "
+    % draw between-level structural model
+    \\node [latent] (mu_1) {$\\mu_{1,t}$};
+    \\node [latent] (phi_1) [right = 1.5em of mu_1] {$\\phi_{1}$};
+    \\node [latent] (lnsigma2_1) [right = 1.5em of phi_1] {$\\log(\\pi_{1})$};
+    "
+    # draw (co-)variances conditional on existing correlations
+    if (nrow(model[model$Param == "r_mu_1.phi_11"]) > 0) {
+      r_mu_1.phi_11 <- "\\draw [cov] (mu_1.north) to node [] {} (phi_1.north);"
+    }
+    if (nrow(model[model$Param == "r_mu_1.ln.sigma2_1"]) > 0) {
+      r_mu_1.ln.sigma2_1 <- "\\draw [cov] (mu_1.north) to node [] {} (lnsigma2_1.north);"
+    }
+    if (nrow(model[model$Param == "r_phi_11.ln.sigma2_1"]) > 0) {
+      r_phi_11.ln.sigma2_1 <- "\\draw [cov] (phi_1.north) to node [] {} (lnsigma2_1.north);"
+    }
+    covs <- paste(r_mu_1.phi_11, r_mu_1.ln.sigma2_1, r_phi_11.ln.sigma2_1, collapse = "")
+  } else { # for q > 1
+    all_bpars <- model[grepl("Fix", model$Type) & model$isRandom == 1, ]
+    # delete this if parameter names with underscore are implemented
+    # replaces dots with nothing
+    all_bpars$Param <- gsub("\\.", "", all_bpars$Param)
+    names_bpars <- gsub(
+      # replace underscore digit with latex subscript
+      "()_(\\d+)", "\\1_{\\2}", gsub(
+        # replace lnsigma with log(pi)
+        "(lnsigma2|lnsigma)_(\\d+)", "log(\\\\pi_{\\2})", all_bpars$Param
+      )
+    )
+    n_bpars <- nrow(all_bpars)
+    bpars_vec <- c()
+    for (i in 1:n_bpars) {
+      if (i == 1) {
+        bpars_vec <- c(
+          bpars_vec, paste0(
+            "\\node  [latent", ifelse(
+              # draw in grey if parameter is fixed
+              all_bpars[i, "isRandom"] == 0,
+              ", color = grey]  (", "]  ("
+            ),
+            all_bpars[i, "Param"], ")  {$\\", names_bpars[i], "$};\n"
+          )
+        )
+      } else {
+        bpars_vec <- c(
+          bpars_vec, paste0(
+            "\\node  [latent", ifelse(
+              # draw in grey if parameter is fixed
+              all_bpars[i, "isRandom"] == 0,
+              ", color = grey]  (", "]  ("
+            ),
+            all_bpars[i, "Param"], ")  [right = 1em of ", all_bpars[i - 1, "Param"],
+            "]  {$\\", names_bpars[i], "$};\n"
+          )
+        )
+      }
+    }
+    bm <- paste(bpars_vec, collapse = "")
+
+
+
+
+    # "
+    # % draw between-level structural model
+    # \\node  [latent]  (mu_y1)  {$\\mu_{y_1,t}$};
+    # \\node  [latent]  (phi_y1)  [right = 1.5em of mu_y1]  {$\\phi_{y_1}$};
+    # \\node  [latent]  (mu_y1)  [right = 1.5em of phi_y1]  {$log(\\pi_{y_1})$};
+    #
+    # "
+    # paste("
+    # % draw nodes
+    # \\foreach \\i [remember = \\i as \\lasti (initially 1)] in {2, ..., ",
+    # VARmodel$q, "}
+    # \\node [manifest] (y\\i t) [right = 2.5em of y\\lasti t] {$y_{\\i,t}$};
+    #
+    # \\foreach \\i [remember = \\i as \\lasti (initially 1)] in {2, ..., ",
+    # VARmodel$q, "}
+    # \\node [latent] (y\\i wt) [above = 2.5em of y\\i t] {$y_{\\i,t}^w$};
+    #
+    # \\foreach \\i [remember = \\i as \\lasti (initially 1)] in {2, ...,",
+    # VARmodel$q, "}
+    # \\node [latent] (mu_y\\i) [below = 2.5em of y\\i t] {$\\mu_{y_{\\i,t}}$};
+    #
+    # % draw paths
+    # \\foreach \\i [remember = \\i as \\lasti (initially 1)] in {1, ...,",
+    # VARmodel$q, "}
+    # \\draw [path] (y\\i wt) to node [] {} (y\\i t);
+    #
+    # \\foreach \\i [remember = \\i as \\lasti (initially 1)] in {1, ...,",
+    # VARmodel$q, "}
+    # \\draw [path] (mu_y\\i) to node [] {} (y\\i t);
+    # "
+    # )
+  }
+
+  # paste together
+  between_model <- paste(bm,
+                         # covs,
+                         sep = "\n")
+
+  # paste together
+  between_model <- paste(
+    begin_figure,
+    bm_caption,
+    begin_tikz,
+    between_model,
+    end_tikz,
+    end_figure,
+    sep = "\n"
+  )
+
+  # paste within_model to markdown
+  cat(between_model, file = "pathmodel.rmd", append = TRUE)
 
 
 
