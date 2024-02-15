15998261637e684904b691bd9e3b64fa1ae37e6b
munchfab <fabian.felix.muench@uni-jena.de>
2024-02-15 14:34:37 +0100

    Include further unit tests for between-model parameters

--- tests/testthat/test-VARmodelBuild.R
+++ tests/testthat/test-VARmodelBuild.R
@@ -1,11 +1,22 @@
 test_that("VARmodelBuild returns correct number of parameters", {
   # test number of returned model parameters for up to 10 constructs
   for (i in 1:10) {
-    test_model <- VARmodelBuild(q = i)
-    n_within_pars <- 2*i + i^2 + ((i * (i - 1)) / 2)
+    test_model <- suppressMessages(VARmodelBuild(q = i))
+    n_within_pars <- 2*i + i^2 + (i * (i - 1)) / 2
+    n_between_pars <- n_within_pars + (n_within_pars * (n_within_pars - 1)) / 2
+    # number of within parameters
     expect_equal(
-      nrow(test_model[test_model$Level == "Within", ]),
+      nrow(test_model[
+        test_model$Model == "Structural" & test_model$Level == "Within",
+      ]),
       n_within_pars
+    )
+    # number of between parameters
+    expect_equal(
+      nrow(test_model[
+        test_model$Model == "Structural" & test_model$Level == "Between",
+      ]),
+      n_between_pars
     )
   }
 })
