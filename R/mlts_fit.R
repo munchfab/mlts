d365e5c6d2c550a8ac04835f57589c96d2aee193
Kenneth Koslowski <kenneth-koslowski@gmx.de>
2024-02-27 10:42:18 +0100

    test renaming function

--- R/mlts_fit.R
+++ R/mlts_fit.R
@@ -24,7 +24,7 @@
 #' @return An object of class `data.frame`.
 #' @export
 #'
-VARfit <- function(VARmodel,
+mlts_fit <- function(VARmodel,
                    data =NULL,
                    id,
                    ts.ind,
