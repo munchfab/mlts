6621b9ae95bc528801ac8d5afd13b8b5c1dcf7bc
munchfab <fabian.felix.muench@uni-jena.de>
2024-04-03 17:23:50 +0200

    Create testthat-file for model formula

---
+++ tests/testthat/test-mlts_model_formula.R
@@ -0,0 +1,18 @@
+test_that("Common formulas are rendered without errors", {
+  # 1 construct, 1 indicator, no lagged effects
+  model <- mlts_model(q = 1)
+  expect_no_error(
+    mlts_model_formula(model)
+  )
+  # 2 construct, 3 indicator, no lagged effects
+  model <- mlts_model(q = 1, p = 3)
+  expect_no_error(
+    mlts_model_formula(model)
+  )
+  # 2 constructs, 1 + 3 indicators, AR2-effects
+  model <- mlts_model(q = 2, p = c(1, 3), max_lag = 2)
+  expect_no_error(
+    mlts_model_formula(model)
+  )
+  file.remove(c("formula.rmd", "formula.pdf"))
+})
