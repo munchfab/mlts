af02b372ef112301023e964e87b8dffbad7bee5f
munchfab <fabian.felix.muench@uni-jena.de>
2024-04-10 15:05:38 +0200

    Alternative create_missings() approach

--- R/create_missings.R
+++ R/create_missings.R
@@ -112,3 +112,94 @@ create_missings <- function(data, tinterval, id, time,
 
   return(new_data)
 }
+
+
+#' Create Missings for Approximation of Continuous Time Dynamic Models
+#' (alternative version)
+#'
+#' @param data An object of class `data.frame` (or one that can be coerced
+#' to that class) containing data of all variables used in the model.
+#' @param tinterval The step interval for approximation for a continuous time DSEM.
+#' The smaller the step interval, the better the approximation.
+#' @param id The variable in `data` that identifies the person or observational
+#' unit (as character).
+#' @param time The variable in `data` that contains the (continuous) time (as
+#' string).
+#' @param btw_vars The names of between-level variables in the data to be
+#' added in newly created rows with NAs.
+#' @param clean Logical. Remove helper columns. Defaults to `TRUE`.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+create_missings2 <- function(data, tinterval, id, time,
+                             btw_vars = NULL, clean = TRUE) {
+
+  # create empty list for imputed data frames
+  imp_list <- list()
+  # create numeric id variable
+  data$num_id <- as.numeric(as.factor(data[, id]))
+
+  # remove between-level variables to add them later again
+  btw_vars <- c(id, btw_vars)
+  btw_data <- unique(data[, c("num_id", btw_vars)])
+  data[, btw_vars] <- NULL
+
+  for (i in 1:length(unique(data$num_id))) {
+
+    # id
+    num_id <- i
+
+    # store continuous time variable for id == i
+    time_cont <- data[data$num_id == i, time]
+
+    # seq in steps of time grid from min to max of time
+    time_seq <- seq(min(time_cont), max(time_cont), by = tinterval)
+
+    # indicate shift interval
+    shifts <- findInterval(x = time_cont, vec = time_seq)
+    max_shifts <- max(table(shifts))
+
+    # print warning if max_shifts increases 4
+    if (max_shifts > 4) {
+      warn_id <- btw_data[btw_data$num_id == i, id]
+      warning("Observations shifted ", max_shifts, " intervals for id ", warn_id)
+    }
+
+    # create empty vector of imputed time
+    time_imp <- NA
+
+    # iterate through values of time_cont
+    for (j in 1:length(time_cont)) {
+      # fill time_imp with values of time_cont
+      time_imp[
+        # determine in which interval between two numbers of time_seq
+        # lies value [j] of time_cont
+        findInterval(x = time_cont[j], vec = time_seq)
+      ] <- time_cont[j] # and fill that position with with value [j]
+    }
+
+    # create integer time variable for use in dsem analysis
+    int_time <- seq_along(time_imp)
+    # store data frame for id == i in imp_list[[i]]
+    imp_list[[i]] <- data.frame(
+      time_imp, int_time,
+      num_id = rep(num_id, times = length(int_time))
+    )
+    colnames(imp_list[[i]]) <- c(time, "int_time", "num_id")
+
+  }
+  # reduce to data frame
+  imp_data <- do.call("rbind", imp_list)
+
+  # join with existing data set
+  # with base R, somewhat slower than with dplyr
+  new_data <- merge(x = imp_data, y = data, by = c("num_id", time), all = T)
+  # sort by num_id and int_time
+  new_data <- new_data[order(new_data$num_id, new_data$int_time), ]
+  # add between-level variables again
+  new_data <- merge(
+    x = new_data, y = btw_data, by = c("num_id"), all.x = T, sort = F
+  )
+}
